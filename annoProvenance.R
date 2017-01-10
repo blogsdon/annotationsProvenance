77818d37c74f2cdbcf89708ea109cc6522d9f15c
Benjamin Logsdon <ben.logsdon@sagebase.org>
2017-01-10 12:31:27 -0800

    updated functionality

--- annoProvenance.R
+++ annoProvenance.R
@@ -2,6 +2,14 @@ require(synapseClient)
 synapseLogin()
 require(rSynapseUtilities)
 
+
+parseGithubLink <- function(x){
+  x <- gsub('\\.','_',x)
+  foo <- strsplit(x,'/')[[1]]
+  return(paste(foo[c(3,4,5,7,8)],collapse='-'))
+}
+
+
 permLink =githubr::getPermlink('blogsdon/annotationsProvenance',
                                'annoProvenance.R',
                                ref = 'branch',
@@ -15,7 +23,7 @@ View(parentDf)
 dummyAnno <- list(a = 'b', 
                   c = 'd',
                   e = 'f')
-dummyAnno <- c(dummyAnno2,setNames(list('executed'),paste(memCompress(permLink),collapse='')))
+dummyAnno <- c(dummyAnno,setNames(list('executed'),parseGithubLink(permLink)))
 
 parentDfObj <- rSynapseUtilities::pushDf2Synapse(parentDf,
                               'parentDf.csv',
@@ -28,7 +36,7 @@ dummyAnno1 <- list(a = 'b',
                    c= 'd', 
                    e = 'f')
 dummyAnno1 <- c(dummyAnno1,setNames(list('used'),synapseClient::synGetProperties(parentDfObj)$id))
-dummyAnno1 <- c(dummyAnno1,setNames(list('executed'),paste(memCompress(permLink),collapse='')))
+dummyAnno1 <- c(dummyAnno1,setNames(list('executed'),parseGithubLink(permLink)))
 
 childDf1 <- data.frame(foo = rnorm(20),
                        bar = rnorm(20),
@@ -46,7 +54,7 @@ dummyAnno2 <- list(a = 'b',
                    e = 'f')
 dummyAnno2 <- c(dummyAnno2,setNames(list('used'),synapseClient::synGetProperties(parentDfObj)$id))
 dummyAnno2 <- c(dummyAnno2,setNames(list('used'),synapseClient::synGetProperties(childDf1Obj)$id))
-dummyAnno2 <- c(dummyAnno2,setNames(list('executed'),paste(memCompress(permLink),collapse='')))
+dummyAnno2 <- c(dummyAnno2,setNames(list('executed'),parseGithubLink(permLink)))
 
 childDf2 <- data.frame(foo = rnorm(20),
                        bar = rnorm(20),
@@ -57,3 +65,4 @@ childDf2Obj <- rSynapseUtilities::pushDf2Synapse(childDf2,
                                                  'childDf2.csv',
                                                  dummyAnno2,
                                                  'syn8007196')
+
